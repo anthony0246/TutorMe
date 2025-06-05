5ae4b9da50b9a2301bfff2cf263690100c229286
RodricDib06 <rodric.dib@gmail.com>
2025-06-04 20:15:52 -0400

    Fixed a routing issue

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,33 +4,35 @@ import AppNavbar from './Navbar';
 import OurOfferings from './OurOfferings';
 import Footer from './Footer';
 import AboutUs from './AboutUs';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // if you route
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HeroSection from './HeroSection';
 import HomePageCards from './HomePageCards';
 
 function App() {
-    return (
+  return (
     <BrowserRouter>
       <div className="d-flex flex-column min-vh-100">
-      <AppNavbar/>          {/* appears on every page */}
-      <HeroSection/>
-      <HomePageCards />
+        <AppNavbar />
         <div className="flex-grow-1">
           <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <HeroSection />
+                  <HomePageCards />
+                </>
+              }
+            />
             <Route path="/offerings" element={<OurOfferings />} />
             <Route path="/aboutus" element={<AboutUs />} />
-            {/* More routes can go here */}
+            {/* Add more routes as needed */}
           </Routes>
         </div>
-        <Footer /> 
+        <Footer />
       </div>
-      
-      <Routes>               {/* your routes here */}
-        {/* <Route path="/" element={<Home />} /> */}
-      </Routes>
     </BrowserRouter>
   );
 }
-
 
 export default App;
