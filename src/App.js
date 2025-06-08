5a4d2464a29396c323561cc53793099039d557dc
Anthony Alam <anthony.alam2005@gmail.com>
2025-06-08 18:57:05 -0400

    BrowserRouter modifications

--- src/App.js
+++ src/App.js
@@ -5,36 +5,33 @@ import OurOfferings from './OurOfferings';
 import Footer from './Footer';
 import AboutUs from './AboutUs';
 import BookSession from './BookSession';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HeroSection from './HeroSection';
 import HomePageCards from './HomePageCards';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div className="d-flex flex-column min-vh-100">
-        <AppNavbar />
-        <div className="flex-grow-1">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <HeroSection />
-                  <HomePageCards />
-                </>
-              }
-            />
-            <Route path="/offerings" element={<OurOfferings />} />
-            <Route path="/aboutus" element={<AboutUs />} />
-            <Route path="/booksession" element={<BookSession />} />
-            {/* Add more routes as needed */}
-          </Routes>
-        </div>
-        <Footer />
+    <div className="d-flex flex-column min-vh-100">
+      <AppNavbar />
+      <div className="flex-grow-1">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <HeroSection />
+                <HomePageCards />
+              </>
+            }
+          />
+          <Route path="/offerings" element={<OurOfferings />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/booksession" element={<BookSession />} />
+          {/* Add more routes as needed */}
+        </Routes>
       </div>
-    </BrowserRouter>,
-    document.getElementById("root")
+      <Footer />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -9,13 +9,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
